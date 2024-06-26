28f1ebf92648b860ad50dc035c85493b794d3d69
Ashaba Jasper <ashabajasper@gmail.com>
2024-06-26 20:44:11 +0300

    CALCULATOR

--- dart_application_1/bin/dart_application_1.dart
+++ dart_application_1/bin/dart_application_1.dart
@@ -1,3 +1,55 @@
+import 'dart:io';
+
 void main() {
-  print('Hello, Dart!');
+  print('Welcome to the Simple Calculator!');
+  print('Please select an operation:');
+  print('1. Addition');
+  print('2. Subtraction');
+  print('3. Multiplication');
+  print('4. Division');
+  print('5. Exit');
+
+  while (true) {
+    stdout.write('Enter your choice (1-5): ');
+    String choice = stdin.readLineSync()!;
+
+    if (choice == '5') {
+      print('Exiting the calculator. Goodbye!');
+      break;
+    }
+
+    stdout.write('Enter the first number: ');
+    double num1 = double.parse(stdin.readLineSync()!);
+
+    stdout.write('Enter the second number: ');
+    double num2 = double.parse(stdin.readLineSync()!);
+
+    double result;
+
+    switch (choice) {
+      case '1':
+        result = num1 + num2;
+        print('Result: $num1 + $num2 = $result');
+        break;
+      case '2':
+        result = num1 - num2;
+        print('Result: $num1 - $num2 = $result');
+        break;
+      case '3':
+        result = num1 * num2;
+        print('Result: $num1 * $num2 = $result');
+        break;
+      case '4':
+        if (num2 != 0) {
+          result = num1 / num2;
+          print('Result: $num1 / $num2 = $result');
+        } else {
+          print('Error: Division by zero is not allowed.');
+        }
+        break;
+      default:
+        print('Invalid choice. Please enter a number between 1 and 5.');
+    }
+    print('');
+  }
 }
