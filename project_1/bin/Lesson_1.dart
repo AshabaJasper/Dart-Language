a2bd582050251a031f9397a2fa32444190dd4274
Ashaba Jasper <ashabajasper@gmail.com>
2024-06-27 06:22:16 +0300

    Update to first Dart Program

---
+++ project_1/bin/Lesson_1.dart
@@ -0,0 +1,7 @@
+// This is a single-line comment. Comments are ignored by the compiler and used for documentation.
+
+// The main function is the entry point of a Dart program.
+void main() {
+  // Print is a built-in function that prints text to the console.
+  print('Hello, World!');
+}
